b9d42b31d66a8fe8ca6f62a7ea9283183a09b5f5
Nikhil <nikhilbagri18@gmail.com>
2023-03-19 04:39:30 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import './Memorygame.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const MemoryGame = () => {
+  const cards = [    '🐶',    '🐱',    '🐭',    '🐹',    '🐰',    '🦊',    '🐻',    '🐼', '🤖' ];
+
+  const [cardsState, setCardsState] = useState(
+    cards.concat(cards).sort(() => Math.random() - 0.5)
   );
-}
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [score, setScore] = useState(0);
 
-export default App;
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+      const [firstCard, secondCard] = flippedCards;
+      if (cardsState[firstCard] === cardsState[secondCard]) {
+        setScore(score + 1);
+        setCardsState(cardsState.filter((_, index) => index !== firstCard && index !== secondCard));
+        setFlippedCards([]);
+      } else {
+        setTimeout(() => setFlippedCards([]), 1000);
+      }
+    }
+  }, [cardsState, flippedCards, score]);
+
+  const handleCardClick = (index) => {
+    if (flippedCards.length < 2 && !flippedCards.includes(index)) {
+      setFlippedCards([...flippedCards, index]);
+    }
+  };
+
+  const handleReset = () => {
+    setCardsState(cards.concat(cards).sort(() => Math.random() - 0.5));
+    setScore(0);
+    setFlippedCards([]);
+  };
+
+  return (
+    <>
+   <div className="d-flex flex-column justify-content-center w-100 h-100">
+  <div className="d-flex flex-column justify-content-center align-items-center">
+    <div className="btn-group my-5">
+      <a href="https://codepen-api-export-production.s3.us-west-2.amazonaws.com/zip/PEN/pyBNzX/1578778289271/pure-css-gradient-background-animation.zip">.</a>
+    </div>
+  </div>
+</div>
+
+    <div className="memory-game">
+      <h1>Memory Game</h1>
+      <div className="score">Score: {score}</div>
+      <div className="cards-container">
+        {cardsState.map((card, index) => (
+          <div
+            key={index}
+            className={`card ${flippedCards.includes(index) ? 'flipped' : ''}`}
+            onClick={() => handleCardClick(index)}
+          >
+            {flippedCards.includes(index) ? card : ''}
+          </div>
+        ))}
+      </div>
+      {cardsState.length === 0 && (
+        <button className="reset-button" onClick={handleReset}>
+          Play again
+        </button>
+      )}
+    </div>
+    </> 
+  );
+};
+
+export default MemoryGame;
